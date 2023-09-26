9accfb5a34a44fae03344dafcc1930bec2819a06
Manoj <hmanoj12@gmail.com>
2023-09-26 18:10:50 +0530

    Test class to testing features

---
+++ src/main/java/dev/manoj/productcatalog/TestingFeatures.java
@@ -0,0 +1,10 @@
+package dev.manoj.productcatalog;
+
+import java.util.List;
+
+public class TestingFeatures {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1,2,3);
+        System.out.println(list.getClass().getName());
+    }
+}
