400d493f7992a56e065d0f14ce5a61903f429901
Manoj <hmanoj12@gmail.com>
2023-10-10 18:58:37 +0530

    Representing Single Table inheritance

--- src/main/java/dev/manoj/productcatalog/InheritanceExample/SingleClass/Mentor.java
+++ src/main/java/dev/manoj/productcatalog/InheritanceExample/SingleClass/Mentor.java
@@ -1,10 +1,14 @@
 package dev.manoj.productcatalog.InheritanceExample.SingleClass;
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity(name="SINGLE_TABLE_MENTOR")
+@DiscriminatorValue("2")
 public class Mentor extends User {
     private Integer noOfSessions;
     private Integer noOfMentees;
--- src/main/java/dev/manoj/productcatalog/InheritanceExample/SingleClass/TA.java
+++ src/main/java/dev/manoj/productcatalog/InheritanceExample/SingleClass/TA.java
@@ -1,11 +1,15 @@
 package dev.manoj.productcatalog.InheritanceExample.SingleClass;
 
 
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity(name="SINGLE_TABLE_TA")
+@DiscriminatorValue("1")
 public class TA extends User {
     private Double rating;
 }
